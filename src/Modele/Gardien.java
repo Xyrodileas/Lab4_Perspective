e2a91f053e41d3d7c46a89de0592af9a4f05e191
i7-9 <i7.aissou@gmail.com>
2014-03-19 21:36:41 -0400

    uml

--- src/Modele/Gardien.java
+++ src/Modele/Gardien.java
@@ -1,4 +1,14 @@
 package Modele;
+/******************************************************
+Cours:  LOG121
+Projet: laboratoire 4
+Nom du fichier: Gardien.java
+Date créé: 2014-03-15
+
+ *******************************************************
+ *@author Alexis Vuillaume, David Murat, Idriss Aissou,
+ *@date Hiver 2014
+ *******************************************************/
 
 import java.util.ArrayList;
 import java.util.List;
