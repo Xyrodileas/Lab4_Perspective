1b0f0e554c258d4af4df7570fa863229a134ad83
Davmira <davmurat@msn.com>
2014-03-22 13:41:41 -0400

    Création controleur translation

---
+++ src/Controleur/Translation.java
@@ -0,0 +1,29 @@
+package Controleur;
+
+public class Translation {
+
+	int x, y;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public void setTranslation(int xModifie, int yModifie) {
+
+		x -= xModifie;
+		y -= yModifie;
+	}
+
+}
