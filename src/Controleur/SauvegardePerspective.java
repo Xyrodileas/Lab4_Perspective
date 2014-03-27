f365fd8e9a0a28458aac2769d6a7474ea3acbad1
i7-9 <i7.aissou@gmail.com>
2014-03-27 18:21:58 -0400

    ;

---
+++ src/Controleur/SauvegardePerspective.java
@@ -0,0 +1,13 @@
+package Controleur;
+
+import Vue.PanneauImage;
+
+public class SauvegardePerspective extends Commande {
+
+	
+	public void execution(PanneauImage image) {
+		image.getPerspective().addDansSauvegardePop(image.getPerspective());
+		
+	}
+
+}
