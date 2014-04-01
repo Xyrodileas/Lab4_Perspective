c5fb283b73255c42af1bb5eeb919c6ce5c79d21b
xyrodileas <xyrodileas@gmail.com>
2014-04-01 14:06:02 -0400

    Ajout pattern NullPerspective

---
+++ src/Modele/NullPerspective.java
@@ -0,0 +1,94 @@
+package Modele;
+
+/**
+ * Created by Alexis on 01/04/14.
+ */
+public class NullPerspective extends Perspective {
+
+    public NullPerspective(int x, int y){
+        super(x, y);
+    }
+
+    public NullPerspective(int PosX, int PosY, double zoom) {
+        super(PosX, PosY, zoom);
+    }
+
+    public NullPerspective(){
+    }
+
+
+    @Override
+    public void incrementeZoom() {
+
+    }
+
+    @Override
+    public void decrementeZoom() {
+
+    }
+
+    @Override
+    public int getHauteurImage() {
+        return 0;
+    }
+
+    @Override
+    public int getLargeurImage() {
+        return 0;
+    }
+
+    @Override
+    public double getZoom() {
+        return 0;
+    }
+
+    @Override
+    public int getPositionX() {
+        return 0;
+    }
+
+    @Override
+    public int getPositionY() {
+       return 0;
+    }
+
+    @Override
+    public void setPositionX(int deplacementRecuX) {
+
+    }
+
+    @Override
+    public void setPositionY(int deplacementRecuY) {
+
+    }
+
+    @Override
+    public void restorePerspective(Perspective snap) {
+
+    }
+
+    @Override
+    public void lastSnap() {
+
+    }
+
+    @Override
+    public void nextSnap() {
+
+    }
+
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void collerUnePerspective(int[] tab) {
+
+    }
+
+    @Override
+    public Perspective clone() {
+       return null;
+    }
+}
